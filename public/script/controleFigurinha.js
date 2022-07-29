bf1f52859357363f837cf7f4c0f8cb7948cbdf11
Matheus Abrahão <matheus.alvares23@outlook.com>
2022-07-29 11:26:08 -0300

    new figurinha card style

---
+++ public/script/controleFigurinha.js
@@ -0,0 +1,5 @@
+let backgroundCard = document.getElementsByClassName("backgroundCard");
+let numeroFigurinha = document.getElementsByClassName("numeroFigurinha");
+
+let tenho = document.getElementsByClassName("tenhoFigurinha");
+let falta = document.getElementsByClassName("faltaFigurinha");
